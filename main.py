521154f0aabf5995995dc94fbc9700fa18ad2692
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2025-08-20 12:58:50 +1000

    Added sys.argv to main.py

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 from stats import get_num_words
 from stats import get_num_characters
 from stats import get_num_characters_sorted
+import sys
 
 def get_book_txt(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-    return file_contents
+    try:
+        with open(file_path, "r") as f:
+            file_contents = f.read()
+        return file_contents
+    except FileNotFoundError:
+        print(f"The file '{file_path}' was not found.")
+        sys.exit(1)
+    except IOError:
+        print(f"An error occurred opening {file_path}")
+        sys.exit(1)
 
 def main():
-    file_path = 'books/frankenstein.txt'
-    text = (get_book_txt(file_path))
-    
-    print(f"============ BOOKBOT ============")
-    print(f"Analyzing book found at {file_path}...")
-    
-    print(f"----------- Word Count ----------")
-    print(f"Found {get_num_words(text)} total words")
-    
-    print(f"--------- Character Count -------")
-    for each in get_num_characters_sorted(get_num_characters(text)):
-        print(f"{each['char']}: {each['num']}")
-    
+ 
+    if len(sys.argv) == 2:
+        file_path = ""
+
+        file_path = sys.argv[1]
+        text = (get_book_txt(file_path))
+        print(f"============ BOOKBOT ============")
+        print(f"Analyzing book found at {file_path}...")
+        
+        print(f"----------- Word Count ----------")
+        print(f"Found {get_num_words(text)} total words")
+        
+        print(f"--------- Character Count -------")
+        for each in get_num_characters_sorted(get_num_characters(text)):
+            print(f"{each['char']}: {each['num']}")
+        
+        sys.exit(0)
+
+    else:
+        print("Usage: python3 main.py <path_to_book>") 
+        sys.exit(1)
+        
+
 main()
